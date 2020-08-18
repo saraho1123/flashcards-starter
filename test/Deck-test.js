d3418a51ce7415b15ce02afe8950a6c4b8d4c4a8
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-08-18 17:13:55 -0600

    Change test description to indicate that a single argument is passed in Deck instantiation

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,19 +6,19 @@ const Deck = require('../src/Deck');
 
 describe('Deck', () => {
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     const deck = new Deck();
 
     expect(Deck).to.be.a('function');
   });
 
-  it.skip('should be an instance of Deck', () => {
+  it('should be an instance of Deck', () => {
     const deck = new Deck();
 
     expect(deck).to.be.an.instanceOf(Deck);
   });
 
-  it.skip('should take an array of Cards as first argument', () => {
+  it('should take an array of Cards as an argument', () => {
     const card1 = new Card(7, 'Which array prototype is not an accessor method?', ['join()', 'slice()', 'splice()'], 'splice()');
     const card2 = new Card(6, 'What is an example of a mutator method?', ['sort()', 'map()', 'join()'], 'sort()')
     const card3 = new Card(20, 'forEach() returns an array', ['true', 'false'], 'false')
@@ -28,7 +28,7 @@ describe('Deck', () => {
     expect(deck.allCards).to.deep.equal([card1, card2, card3]);
   });
 
-  it.skip('should count how many Cards are in the Deck', () => {
+  it('should count how many Cards are in the Deck', () => {
     const card1 = new Card(7, 'Which array prototype is not an accessor method?', ['join()', 'slice()', 'splice()'], 'splice()');
     const card2 = new Card(6, 'What is an example of a mutator method?', ['sort()', 'map()', 'join()'], 'sort()')
     const card3 = new Card(20, 'forEach() returns an array', ['true', 'false'], 'false')
