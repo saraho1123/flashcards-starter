a6732738fef37435fce9ba4985d35d3b5fde44bc
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-08-20 13:40:30 -0600

    Add currentRound as property on Game and create start()

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,14 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('./Card');
+const Round = require('./Round');
+const Deck = require('./Deck');
+
 class Game {
-  constructor() {}
+  constructor(currentRound = null) {
+    this.currentRound = currentRound;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +18,15 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    const myCards = prototypeQuestions.map(question => {
+      let card = new Card(question.id, question.question, question.answers, question.correctAnswer);
+      return card;
+    })
+    const deck = new Deck(myCards);
+    this.currentRound = new Round(deck);
   }
 }
 
