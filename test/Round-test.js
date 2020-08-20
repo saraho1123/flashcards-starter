92a15aec0beafa9bfe0b418360517f078bcda2cb
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-08-19 21:08:48 -0600

    Add tests for Round to verify functionality and data

--- test/Round-test.js
+++ test/Round-test.js
@@ -25,76 +25,83 @@ describe('Round', () => {
     round = new Round(deck);
   });
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     expect(Round).to.be.a('function');
   });
 
-  it.skip('should be an instance of Round', () => {
+  it('should be an instance of Round', () => {
     expect(round).to.be.an.instanceOf(Round);
   });
 
-  it.skip('should take a deck as an argument', () => {
+  it('should take a deck as an argument', () => {
     expect(round.deck).to.deep.equal(deck);
   });
 
-  it.skip('should have turns set to 0 by default', () => {
+  it('should have turns set to 0 by default', () => {
     expect(round.turns).to.equal(0);
   });
 
-  // it('should create a new Turn when guess is made', () => {
-  //   round.takeTurn('pug');
-  //   expect(round.takeTurn()).to.equal();
-  // });
+  it('should have incorrect guesses set to empty by default', () => {
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
 
-  it.skip('should give feedback based on accuracy of guess', () => {
+  it('should indicate which card is currently in play', () => {
+    round.returnCurrentCard();
+    expect(round.returnCurrentCard()).to.deep.equal(card1);
+  });
+
+  it('should increase number of turns when a turn is taken', () => {
+    expect(round.turns).to.equal(0);
+
     round.takeTurn('pug');
+    expect(round.turns).to.equal(1);
 
+    round.takeTurn('dog');
+    round.takeTurn('playing with bubble wrap');
+
+    expect(round.turns).to.equal(3);
+  });
+
+  it('should update current card with next card', () => {
+    round.takeTurn('pug');
+    expect(round.returnCurrentCard()).to.deep.equal(card2);
+
+    round.takeTurn('gallbladder');
+    expect(round.returnCurrentCard()).to.deep.equal(card3);
+  });
+
+  it('should store incorrect guesses', () => {
+    round.takeTurn('pug');
+    expect(round.incorrectGuesses).to.deep.equal([round.deck.allCards[0].id]);
+
+    round.takeTurn('log');
+    expect(round.incorrectGuesses).to.deep.equal([1, 14]);
+  });
+
+
+  it('should give feedback based on accuracy of guess', () => {
+    round.takeTurn('pug');
     expect(round.takeTurn()).to.equal('incorrect!');
   });
 
-  it.skip('should increase number of turns when a turn is taken', () => {
-    expect(round.turns).to.equal(0);
-
-    round.takeTurn(turn.guess);
-
-    expect(round.turns).to.equal(1);
-
-    round.takeTurn(turn.guess);
-
-    expect(round.turns).to.equal(2);
-  });
-
-  it.skip('should update current card with next card', () => {
+  it('should calculate the percentage of correct guesses', () => {
     round.takeTurn('pug');
+    round.takeTurn('gallbladder');
+    round.takeTurn('watching Netflix');
 
-    expect(turn.currentCard).to.deep.equal(card2);
-  });
-
-  it.skip('should store incorrect guesses', () => {
-    round.takeTurn('pug');
-
-    expect(round.incorrectGuesses).to.deep.equal(round.deck.allCards[1].id);
-  });
-
-  it.skip('should indicate which card is currently in play', () => {
-    round.returnCurrentCard();
-
-    expect(round.returnCurrentCard()).to.deep.equal(turn.currentCard);
-  });
-
-  it.skip('should calculate the percentage of correct guesses', () => {
     round.calculatePercentCorrect();
 
-    expect(round.calculatePercentCorrect()).to.equal('0%');
+    expect(round.calculatePercentCorrect()).to.equal(33);
   });
 
-  it.skip('should show termination message', () => {
+  it('should show termination message', () => {
+    round.takeTurn('pug');
+    round.takeTurn('gallbladder');
+    round.takeTurn('watching Netflix');
+
+    round.calculatePercentCorrect();
     round.endRound();
 
-    expect(round.endRound()).to.equal('Round over! You answered 0% of the questions correctly!');
-  });
-
-  it.skip('should have incorrect guesses set to empty by default', () => {
-    expect(round.incorrectGuesses).to.deep.equal([]);
+    expect(round.endRound()).to.equal('**Round over!** You answered 33% of the questions correctly!');
   });
 });
