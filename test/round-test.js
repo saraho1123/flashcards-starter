ba3fdd13045e04a8e3946c0502601b2ccb7f0f4e
Sarah Osgood <sarahjs117@gmail.com>
2020-10-06 19:12:42 -0500

    Refactor tests for round class using beforeEach()

--- test/round-test.js
+++ test/round-test.js
@@ -7,33 +7,32 @@ const Round = require('../src/round');
 const Turn = require('../src/Turn');
 
 describe('Round', function() {
+  let card1;
+  let decks;
+  let round1;
+
+  beforeEach(function() {
+    card1 = new Card(1, 'What is Isaac\'s favorite color?', ['blue', 'orange', 'black'], 'black');
+    decks = new Deck([card1]);
+    round1 = new Round(decks);
+    round = new Round();
+
+  });
 
   it('should be a function', function() {
-    const round = new Round();
-
     expect(Round).to.be.a('function');
   });
 
   it('should be an instance of Round', function() {
-    const round = new Round();
-
     expect(round).to.be.an.instanceof(Round);
   }); 
 
   it('should return the first Card in the Deck array', function() {
-    const card1 = new Card(1, 'What is Isaac\'s favorite color?', ['blue', 'orange', 'black'], 'black');
-    const decks = new Deck([card1]);
-    const round1 = new Round(decks);
-  
     expect(round1.currentCard).to.be.a('object');
     expect(round1.returnCurrentCard()).to.equal(card1)
   });  
 
   it('should return the first Card in the Deck array', function() {
-    const card1 = new Card(1, 'What is Isaac\'s favorite color?', ['blue', 'orange', 'black'], 'black');
-    const decks = new Deck([card1]);
-    const round1 = new Round(decks);
-  
     expect(round1.currentCard).to.be.a('object');
     expect(round1.returnCurrentCard()).to.equal(card1)
   }); 
