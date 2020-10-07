7d36349c3eb606a09184dd9f735b67aec9c57600
Sarah Osgood <sarahjs117@gmail.com>
2020-10-06 19:12:10 -0500

    Refactor tests for turn class using beforeEach

--- test/turn-test.js
+++ test/turn-test.js
@@ -5,52 +5,45 @@ const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe('Turn', function() {
+  let turn;
+  let turn1;
+  let turn2;
+  let card;
+
+  beforeEach(function() {
+    turn = new Turn();
+    turn1 = new Turn('black', card);
+    turn2 = new Turn('blue', card);
+    card = new Card(1, 'What is Isaac\'s favorite color?', ['blue', 'red', 'black'], 'black');
+  });
+
 
   it('should be a function', function() {
-    const turn = new Turn();
-
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', function() {
-    const turn = new Turn();
-
     expect(turn).to.be.an.instanceof(Turn);
   }); 
 
   it('should store an attempted answer', function() {
-    const turn = new Turn('black');
-
-    expect(turn.returnGuess()).to.equal('black');
+    expect(turn1.returnGuess()).to.equal('black');
   });  
 
   it('should store a card object', function() {
-    const card = new Card(1, 'What is Isaac\'s favorite color?', ['blue', 'red', 'black'], 'black');
-    const turn = new Turn('black', card);
-
-    expect(turn.returnCard()).to.be.an.instanceof(Card);
+    expect(turn1.returnCard()).to.be.an.instanceof(Card);
   });  
 
   it('should return true for a correct answer attempt', function() {
-    const card = new Card(1, 'What is Isaac\'s favorite color?', ['blue', 'red', 'black'], 'black');
-    const turn = new Turn('black', card);
-
-    expect(turn.evaluateGuess()).to.equal(true);
+    expect(turn1.evaluateGuess()).to.equal(true);
   }); 
 
   it('should return false for acorrect answer attempt', function() {
-    const card = new Card(1, 'What is Isaac\'s favorite color?', ['blue', 'red', 'black'], 'black');
-    const turn = new Turn('blue', card);
-
-    expect(turn.evaluateGuess()).to.equal(false);
+    expect(turn2.evaluateGuess()).to.equal(false);
   }); 
 
   it('should tell user if their attempt was correct', function() {
-    const card = new Card(1, 'What is Isaac\'s favorite color?', ['blue', 'red', 'black'], 'black');
-    const turn1 = new Turn('blue', card);
-    const turn2 = new Turn('black', card);
-
-    expect(turn1.giveFeedback()).to.equal('incorrect!');
-    expect(turn2.giveFeedback()).to.equal('correct!');
+    expect(turn1.giveFeedback()).to.equal('correct!');
+    expect(turn2.giveFeedback()).to.equal('incorrect!');
   }); 
 });
